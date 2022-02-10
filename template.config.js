21a124f293048c0b76fbf018d3c9eb0720882a51
Muhammad Numan <muhammadnuman70@gmail.com>
2022-02-11 02:04:48 +0500

    chore: add palceholders

--- template.config.js
+++ template.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  // Placeholder used to rename and replace in files
-  // package.json, index.json, android/, ios/
-  placeholderName: "ProjectName",
-
   // Directory with template
   templateDir: "./template",
 
+  placeholders: {
+    hermes_flag:true,
+
+    slug:"my_project_name",
+    
+    // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
+    name: 'MyProjectName',
+
+    // title that will be replaced in values.xml and Info.plist with title provided by the user.
+    // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
+    title: 'Hello App Display Name'
+  },
   // Path to script, which will be executed after init
   postInitScript: "./script.js"
 };
